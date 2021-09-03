072b5003945c6efefbe3d4c293dd851c650b967b
Ramanath <ramanath784@gmail.com>
2021-09-04 02:51:41 +0530

    auth module - password change status code update

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -233,7 +233,7 @@ exports.updatePassword = catchAsync(async (req, res, next) => {
 	const user = await User.findById(req.user.id).select('+password')
 
 	if (!(await user.correctPassword(req.body.passwordCurrent, user.password))) {
-		return next(new AppError('Your current password is incorrect', 401))
+		return next(new AppError('Your current password is incorrect', 400))
 	}
 
 	user.password = req.body.password
